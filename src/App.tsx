3938df469ede5ac39904fbc95a6755172850eb41
eiiir <chrysanthemum49@hotmail.com>
2024-12-25 18:32:24 +0900

    Sync with a doc in Firestore

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,50 @@
-import React from 'react';
+import Firebase from './Firebase';
+import React, { useState, useEffect } from 'react';
 
 const App = () => {
-  return <div>Hello, React with TypeScript and Webpack!</div>;
+  const [currentValue, setCurrentValue] = useState<string>('Loading...');
+  const [lastUpdatedTimestamp, setLastUpdatedTimestamp] = useState<string>('Loading...');
+
+  const fetchInitialValue = async () => {
+    const initialValue = await Firebase.getCurrentValue();
+    setCurrentValue(initialValue?.message ?? "No data found");
+    setLastUpdatedTimestamp(initialValue?.time ?? 'No data found');
+  }
+
+  useEffect(() => {
+    fetchInitialValue();
+    Firebase.listenToUpdate((data) => {
+      setCurrentValue(data?.message as string);
+      setLastUpdatedTimestamp(data?.time as string);
+    });
+  }, []);
+
+  const handleSubmit = (val: string) => {
+    Firebase.updateValue(val);
+  };
+
+  return <div>
+    <div>Hello, React with TypeScript and Webpack!</div>
+    <VisualApp 
+      currentValue={currentValue} 
+      lastUpdatedTimestamp={lastUpdatedTimestamp} 
+      onSubmit={handleSubmit} 
+    />
+  </div>;
 };
+
+interface VisualAppProps {
+  currentValue: string;
+  lastUpdatedTimestamp: string;
+  onSubmit: (val: string) => void;
+}
+
+const VisualApp: React.FC<VisualAppProps> = ({ currentValue, lastUpdatedTimestamp, onSubmit }) => {
+  return <div>
+    <h1>Current Value: {currentValue}</h1>
+    <h1>Last Updated: {lastUpdatedTimestamp}</h1>
+    <input type="text" defaultValue={currentValue} value={currentValue} onChange={(e) => onSubmit(e.target.value)} />
+  </div>;
+}
 
 export default App;
---
+++ src/Firebase.ts
@@ -0,0 +1,47 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, FirebaseApp } from "firebase/app";
+import { getFirestore, doc, onSnapshot, getDoc, updateDoc } from "firebase/firestore";
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBOMeZb5fdX4sWZFfa-qPhNFyTzxMZHc9U",
+  authDomain: "kanshasai-32b7b.firebaseapp.com",
+  projectId: "kanshasai-32b7b",
+  storageBucket: "kanshasai-32b7b.firebasestorage.app",
+  messagingSenderId: "289175026532",
+  appId: "1:289175026532:web:c964064370bcfa1d895871"
+};
+
+// Initialize Firebase
+const fb = initializeApp(firebaseConfig);
+
+const myApp = (fb: FirebaseApp) => {
+    const db = getFirestore(fb);
+    const docRef = doc(db, "testcollection", "kItghYhlImgteWHMLkRT");
+
+
+    return {
+        getCurrentValue: async () => {
+            const docSnap = await getDoc(docRef);
+            if (docSnap.exists()) {
+                return docSnap.data();
+            } else {
+                console.log("No such document!");
+                return null;
+            }
+        },
+        listenToUpdate: (callback: (data: any) => void) => {
+            onSnapshot(docRef, (doc) => {
+                console.log("Current data: ", doc.data());
+                callback(doc.data());
+            });
+        },
+        updateValue: (newValue: string) => {
+            updateDoc(docRef, { message: newValue, time: new Date().toISOString() });
+        }
+    }
+
+}
+
+export default myApp(fb);
